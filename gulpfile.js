e7c9eaf00e49a119f9868b76d53b5eef283657bd
Julia Vangeliy <vangeliy_yulia@mail.ru>
2018-05-13 12:16:16 +0300

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+// Default task
+gulp.task('default', function() {
+    gulp.start('sass:watch');
+});
